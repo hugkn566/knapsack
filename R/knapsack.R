f750dc287cf4aebdb85899f0823e32832f907cbd
Otto Moen <ottmo242@student.liu.se>
2020-10-08 09:48:51 +0200

    Package documentation

---
+++ R/knapsack.R
@@ -0,0 +1,12 @@
+#' knapsack: solving the knapsack problem
+#' 
+#' This package provides three different functions to solve the knapsack problem:
+#' \itemize{
+#'   \item \code{\link{brute_force_knapsack}}
+#'   \item \code{\link{knapsack_dynamic}}
+#'   \item \code{\link{greedy_knapsack}}
+#' }
+#' 
+#' @docType package
+#' @name knapsack
+NULL
